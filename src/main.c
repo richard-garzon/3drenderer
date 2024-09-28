5eda4fe7816e5da6b271a01387496c4068a14a26
rg <garzon.r@gmail.com>
2024-09-28 11:13:59 -0400

    sdl texture

--- src/main.c
+++ src/main.c
@@ -1,30 +1,26 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
-SDL_Window* window = NULL;
-SDL_Renderer* renderer = NULL;
+SDL_Window *window = NULL;
+SDL_Renderer *renderer = NULL;
 bool is_running = false;
+uint32_t *color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
 
-bool initialize_window(void)
-{
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-	{
+bool initialize_window(void) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		fprintf(stderr, "Error initializing SDL.\n");
 		return false;
 	}
 	// Create SDL Window
-	window = SDL_CreateWindow(
-		NULL,
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
-		SDL_WINDOW_BORDERLESS
-	);
+	window =
+		SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+						 window_width, window_height, SDL_WINDOW_BORDERLESS);
 
-	if (!window)
-	{
+	if (!window) {
 		fprintf(stderr, "Error creating SDL Window.\n");
 		return false;
 	}
@@ -32,8 +28,7 @@ bool initialize_window(void)
 	// Create an SDL renderer
 	renderer = SDL_CreateRenderer(window, -1, 0);
 
-	if (!renderer)
-	{
+	if (!renderer) {
 		fprintf(stderr, "Error creating SDL renderer.\n");
 		return false;
 	}
@@ -41,53 +36,55 @@ bool initialize_window(void)
 	return true;
 }
 
-void setup(void)
-{
+void setup(void) {
+	color_buffer = (uint32_t*) malloc(sizeof(uint32_t) * window_width * window_height);
 }
 
-void process_input(void)
-{
+void process_input(void) {
 	SDL_Event event;
 	SDL_PollEvent(&event);
 
-	switch (event.type)
-	{
-		case SDL_QUIT:
+	switch (event.type) {
+	case SDL_QUIT:
+		is_running = false;
+		break;
+	case SDL_KEYDOWN:
+		if (event.key.keysym.sym == SDLK_ESCAPE) {
 			is_running = false;
 			break;
-		case SDL_KEYDOWN:
-			if (event.key.keysym.sym == SDLK_ESCAPE)
-			{
-				is_running = false;
-				break;
-			}
+		}
 	}
 }
 
-void update(void)
-{
-}
+void update(void) {}
 
-void render(void)
-{
+void render(void) {
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
 	SDL_RenderPresent(renderer);
 }
 
-int main(void)
+void destroy_window(void)
 {
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+
+int main(void) {
 	is_running = initialize_window();
 
 	setup();
 
-	while(is_running)
-	{
+	while (is_running) {
 		process_input();
 		update();
 		render();
 	}
+
+	destroy_window();
 
 	return 0;
 }
