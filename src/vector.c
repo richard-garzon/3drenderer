3ba34a4ffcc536fd2b416d6d6169453042a93a19
rg <garzon.r@gmail.com>
2024-11-03 12:55:54 -0500

    vector mul and div

--- src/vector.c
+++ src/vector.c
@@ -15,6 +15,20 @@ vec2_t vec2_subtract(vec2_t a, vec2_t b)
 	return result;
 }
 
+vec2_t vec2_mul(vec2_t v, float factor)
+{
+	vec2_t result = {.x = v.x * factor, .y = v.y * factor};
+
+	return result;
+}
+
+vec2_t vec2_div(vec2_t v, float factor)
+{
+	vec2_t result = {.x = v.x / factor, .y = v.y / factor};
+
+	return result;
+}
+
 float vec3_length(vec3_t v) { return sqrt(v.x * v.x + v.y * v.y + v.z * v.z); }
 
 vec3_t vec3_add(vec3_t a, vec3_t b)
@@ -28,6 +42,21 @@ vec3_t vec3_subtract(vec3_t a, vec3_t b)
 	vec3_t result = {.x = a.x - b.x, .y = a.y - b.y, .z = a.z - b.z};
 	return result;
 }
+
+vec3_t vec3_mul(vec3_t v, float factor)
+{
+	vec3_t result = {.x = v.x * factor, .y = v.y * factor, .z = v.z * factor};
+
+	return result;
+}
+
+vec3_t vec3_div(vec3_t v, float factor)
+{
+	vec3_t result = {.x = v.x / factor, .y = v.y / factor, .z = v.z / factor};
+
+	return result;
+}
+
 vec3_t vec3_rotate_x(vec3_t v, float angle)
 {
 	vec3_t rotated_vector = {.x = v.x,
--- src/vector.h
+++ src/vector.h
@@ -17,10 +17,14 @@ typedef struct
 float vec2_length(vec2_t v);
 vec2_t vec2_add(vec2_t a, vec2_t b);
 vec2_t vec2_subtract(vec2_t a, vec2_t b);
+vec2_t vec2_mul(vec2_t v, float factor);
+vec2_t vec2_div(vec2_t v, float factor);
 
 float vec3_length(vec3_t v);
 vec3_t vec3_add(vec3_t a, vec3_t b);
 vec3_t vec3_subtract(vec3_t a, vec3_t b);
+vec3_t vec3_mul(vec3_t v, float factor);
+vec3_t vec3_div(vec3_t v, float factor);
 
 vec3_t vec3_rotate_x(vec3_t v, float angle);
 vec3_t vec3_rotate_y(vec3_t v, float angle);
